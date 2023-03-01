ed33cc062db2433e1b548c8c1f1467578f269281
lvfvs <70900967+lvfvs@users.noreply.github.com>
2023-03-02 01:01:17 +1100

    Test user data collection using access token

--- index.js
+++ index.js
@@ -69,8 +69,22 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        // Formatting the JSON output
-        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        const { access_token, token_type } = response.data;
+
+        axios
+          .get("https://api.spotify.com/v1/me", {
+            headers: {
+              Authorization: `${token_type} ${access_token}`,
+            },
+          })
+
+          .then((response) => {
+            // Formatting the JSON output
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          })
+          .catch((error) => {
+            res.send(error);
+          });
       } else {
         res.send(response);
       }

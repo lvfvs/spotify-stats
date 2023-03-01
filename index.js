24412d04e1a243507e3bc2ad3f6172b736d9f7da
lvfvs <70900967+lvfvs@users.noreply.github.com>
2023-03-01 23:59:17 +1100

    Set up /callback route handler

--- index.js
+++ index.js
@@ -18,14 +18,39 @@ app.get("/", (req, res) => {
   res.json(data);
 });
 
+const generateRandomisedString = (length) => {
+  let string = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    string += possible.charAt(Match.floor(Math.random() * possible.length));
+  }
+
+  return string;
+};
+
+const stateKey = "spotify_auth_state";
+
 app.get("/login", (req, res) => {
+  const state = generateRandomisedString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
   const queryParameters = querystring.stringify({
     client_id: CLIENT_ID,
     response_type: "code",
     redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
   });
 
   res.redirect(`https://accounts.spotify.com/authorize?${queryParameters}`);
+});
+
+app.get("/callback", (req, res) => {
+  res.send("callback");
 });
 
 app.listen(port, () => {

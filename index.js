3796aa22d617e4b012d01406e3cc6c1cfa5e1440
lvfvs <70900967+lvfvs@users.noreply.github.com>
2023-03-02 00:33:23 +1100

    Implement POST request with axios

--- index.js
+++ index.js
@@ -50,7 +50,34 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/callback", (req, res) => {
-  res.send("callback");
+  const code = req.query.code || null;
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: REDIRECT_URI,
+    }),
+    headers: {
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 app.listen(port, () => {

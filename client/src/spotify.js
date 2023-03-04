d75e9d96d48db02ae49e1546399f7199d42281f9
lvfvs <70900967+lvfvs@users.noreply.github.com>
2023-03-05 09:59:33 +1100

    Add map for local storages keys and values

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,3 +1,17 @@
+const LOCALSTORAGE_KEYS = {
+  accessToken: "spotify_access_token",
+  refreshToken: "spotify_refresh_token",
+  expiryTime: "spotify_token_expiry_time",
+  timestamp: "spotify_token_timestamp",
+};
+
+const LOCALSTORAGE_VALUES = {
+  accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+  refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+  expiryTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expiryTime),
+  timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+};
+
 const getAccessToken = () => {
   const queryString = window.location.search;
   const urlSearchParams = new URLSearchParams(queryString);

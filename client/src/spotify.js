d3be88e364c1a883351f302afcf7826002a563ce
lvfvs <70900967+lvfvs@users.noreply.github.com>
2023-03-06 11:41:09 +1100

    Add function to get the current users' profile information

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -105,3 +105,7 @@ export const logout = () => {
 axios.defaults.baseURL = "https://api.spotify.com/v1";
 axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
 axios.defaults.headers["Content-Type"] = "application/json";
+
+export const getCurrentUserProfile = () => {
+  axios.get("/me");
+};
